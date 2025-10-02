1da2d6f650b47d09493188f7d621693f72d327e8
GabS.Dev <el_santofr@hotmail.com>
2025-10-02 16:43:29 +0200

    2-10 funcionando 3

--- src/Components/PlaceHolder/PlaceHolder.jsx
+++ src/Components/PlaceHolder/PlaceHolder.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './PlaceHolder.css'
+import { GiMagicGate } from "react-icons/gi";
+import { GiPerspectiveDiceSixFacesOne } from "react-icons/gi";
+import { GiPoliceBadge } from "react-icons/gi";
+import { GiNotebook } from "react-icons/gi";
 
 const PlaceHolder = () => {
   return (
     <div className='placeHolder'>
-        <h1 color='red'>PlaceHolder</h1>
+      <span className='placeHolder__Logo'><GiMagicGate size={100} /></span>
+        <h1 className='placeHolder__title'>Bienvenido a RPG Messenger</h1>
+        <p className='placeHolder__text'>Selecciona un contacto para comenzar tu aventura de rol por mensajería</p>
+        <span className='placeHolder__details'><GiPerspectiveDiceSixFacesOne size={15} />  Roll dados  <GiPoliceBadge size={15} />  Ver stats <GiNotebook />  Gestionar campañas</span>
     </div>
   )
 }
